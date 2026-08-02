dc1d7606048c76cce4f502813d7ff8783b196534
caesar722 <caesar722@gmail.com>
2026-08-02 21:54:43 +0900

    店休日表示を共通化

---
+++ store-info.js
@@ -0,0 +1,58 @@
+(function () {
+  'use strict';
+
+  const storeInfo = {
+    addressShort: '大分県大分市都町',
+    open: '10:30〜19:00',
+    holiday: '不定休',
+    holidayMonth: '8月',
+    holidayDates: [
+      '5日',
+      '6日',
+      '14日',
+      '15日',
+      '20日',
+      '21日',
+      '24日',
+      '28日',
+      '29日'
+    ]
+  };
+
+  function renderHolidayDates(element) {
+    element.replaceChildren();
+
+    storeInfo.holidayDates.forEach((date, index) => {
+      const dateItem = document.createElement('span');
+      dateItem.className = 'store-holiday-date-item';
+      dateItem.textContent = date + (index < storeInfo.holidayDates.length - 1 ? '・' : '');
+      element.appendChild(dateItem);
+
+      if (index < storeInfo.holidayDates.length - 1) {
+        element.appendChild(document.createElement('wbr'));
+      }
+    });
+  }
+
+  function renderStoreInfo() {
+    document.querySelectorAll('[data-store-info]').forEach((element) => {
+      const key = element.dataset.storeInfo;
+
+      if (key === 'holidayTitle') {
+        element.textContent = `${storeInfo.holidayMonth}店休日`;
+      } else if (key === 'holidayDates') {
+        renderHolidayDates(element);
+      } else if (Object.prototype.hasOwnProperty.call(storeInfo, key)) {
+        element.textContent = storeInfo[key];
+      }
+    });
+  }
+
+  window.storeInfo = storeInfo;
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', renderStoreInfo);
+  } else {
+    renderStoreInfo();
+  }
+}());
